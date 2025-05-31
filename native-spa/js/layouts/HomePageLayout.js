5d562f6c4983a30087ebb1f9b76ca6e2952e5c75
belal-allala <belalallala810@gmail.com>
2025-05-31 18:58:29 +0100

    refactor: Modified native-spa/js/layouts/HomePageLayout.js

--- native-spa/js/layouts/HomePageLayout.js
+++ native-spa/js/layouts/HomePageLayout.js
@@ -1,51 +1,34 @@
-
-import EmployeeTableLayout from './EmployeeTableLayout.js';
+import SeanceListLayout from './SeanceListLayout.js'; 
 
 const HomePageLayout = () => {
+    return `
+        <h1 class="text-center my-4">Système de Gestion de Tickets de Cinéma</h1>
 
-    return `      <h1 class="text-center">Employee Manager</h1>
-    <!-- Add Employee Button -->
-    <button
-      class="btn btn-primary"
-      data-bs-toggle="modal"
-      data-bs-target="#employeeModal"
-      id="addEmployeeBtn"
-    >
-      Add Employee
-    </button>
-    <!-- Toast Notification -->
-    <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 1055">
-      <div
-        id="liveToast"
-        class="toast"
-        role="alert"
-        aria-live="assertive"
-        aria-atomic="true"
-      >
-        <div class="toast-header">
-          <strong class="me-auto">Notification</strong>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="toast"
-            aria-label="Close"
-          ></button>
+        <!-- Toast Notification (gardé car utile) -->
+        <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 1055">
+          <div
+            id="liveToast"
+            class="toast"
+            role="alert"
+            aria-live="assertive"
+            aria-atomic="true"
+          >
+            <div class="toast-header">
+              <strong class="me-auto">Notification</strong>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="toast"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="toast-body"></div>
+          </div>
         </div>
-        <div class="toast-body"></div>
-      </div>
-    </div>
 
-    <!-- Search Input -->
-    <div class="my-3">
-      <input
-        type="text"
-        id="searchInput"
-        class="form-control"
-        placeholder="Search employee by name"
-      />
-    </div>
-    ${EmployeeTableLayout()}
-    `
+        <!-- Affichage de la liste des séances -->
+        ${SeanceListLayout()}
+    `;
 }
 
 export default HomePageLayout;
